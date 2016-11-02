273d7d3f7c136e4873d6e5ab38dc8c10bffa352f
Dom Rodriguez <shymega@shymega.org.uk>
2016-11-02 08:44:00 +0000

    opcodes: Add more bytecode

--- classfile/src/opcodes/opcodes.rs
+++ classfile/src/opcodes/opcodes.rs
@@ -1,4 +1,11 @@
 pub enum Opcodes {
     AALoad = 0x32,
     AAStore = 0x53,
+    AConstNull = 0x01,
+    ALoad = 0x19,
+    ALoad0 = 0x2a,
+    ALoad1 = 0x2b,
+    ALoad2 = 0x2c,
+    ALoad3 = 0x2d,
+    ANewArray = 0xbd,
 }
