015e44a1963e50b0e724a60689fd9a099a4883f2
Dom Rodriguez <shymega@shymega.org.uk>
2016-11-09 14:36:24 +0000

    Capitialise Opcodes/Bytecodes

--- classfile/src/opcodes/opcodes.rs
+++ classfile/src/opcodes/opcodes.rs
@@ -1,11 +1,12 @@
+#[allow(dead_code)]
 pub enum Opcodes {
-    AALoad = 0x32,
-    AAStore = 0x53,
-    AConstNull = 0x01,
-    ALoad = 0x19,
-    ALoad0 = 0x2a,
-    ALoad1 = 0x2b,
-    ALoad2 = 0x2c,
-    ALoad3 = 0x2d,
-    ANewArray = 0xbd,
+    AALOAD = 0x32,
+    AASTORE = 0x53,
+    ACONSTNULL = 0x01,
+    ALOAD = 0x19,
+    ALOAD0 = 0x2a,
+    ALOAD1 = 0x2b,
+    ALOAD2 = 0x2c,
+    ALOAD3 = 0x2d,
+    ANEWARRAY = 0xbd,
 }
