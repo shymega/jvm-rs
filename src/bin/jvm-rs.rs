93b5624a38efaf732e326b8f26fa712ca3d0d68b
Dom Rodriguez <shymega@shymega.org.uk>
2017-12-23 16:58:20 +0000

    Update code style in src/bin/jvm-rs.rs

--- src/bin/jvm-rs.rs
+++ src/bin/jvm-rs.rs
@@ -31,27 +31,30 @@ fn get_arguments() -> clap::ArgMatches<'static> {
         .version(VERSION)
         .author("Dom Rodriguez <shymega@shymega.org.uk>")
         .about("JVM written in Rust.")
-        .arg(Arg::with_name("log_config")
-            .short("lc")
-            .long("log_config")
-            .value_name("FILE")
-            .help("Sets a configuration file for the logger.")
-            .takes_value(true)
-            .required(false))
-        .arg(Arg::with_name("v")
-            .short("v")
-            .multiple(true)
-            .takes_value(false)
-            .required(false)
-            .help("Sets the level of logging verbosity."))
+        .arg(
+            Arg::with_name("log_config")
+                .short("lc")
+                .long("log_config")
+                .value_name("FILE")
+                .help("Sets a configuration file for the logger.")
+                .takes_value(true)
+                .required(false),
+        )
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .multiple(true)
+                .takes_value(false)
+                .required(false)
+                .help("Sets the level of logging verbosity."),
+        )
         .get_matches()
 }
 
 fn main() {
     let matches = get_arguments();
 
-    let log_config = matches.value_of("log_config")
-        .unwrap_or("./log4rs.yml");
+    let log_config = matches.value_of("log_config").unwrap_or("./log4rs.yml");
 
     init_logger(log_config);
 
